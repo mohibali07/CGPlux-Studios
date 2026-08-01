0364a409e81d3afb0885f7dd9726aeea0126fea2
Syed Mohib <mohibali.tms@gmail.com>
2026-08-01 19:20:59 +0500

    fix: remove useTypeScriptCli from next.config.ts to resolve build error on Linux VPS

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
       },
     ],
-  },
-  experimental: {
-    useTypeScriptCli: true,
   },
 };
 
